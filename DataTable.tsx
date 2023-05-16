03a39e18d7a98becf0e266560f75467437e00fb8
atmelino <2985251+atmelino@users.noreply.github.com>
2023-05-15 19:58:32 -0500

    clean output

--- DataTable.tsx
+++ DataTable.tsx
@@ -18,18 +18,14 @@ export  function DataTable(props: DataTableProps) {
 		<table class="border-2">
 			<tr class="border-2">
 				{keys.map((data, index) => {
-					// console.log(keys[index]);
 					return <th class="border-2">{keys[index]}</th>;
 				})}
 			</tr>
 
 			{props.dataArray.map((data, index) => {
-				// console.log(data);
-				// const keys = Object.keys(data);
 				return (
 					<tr class="border-2" key={index}>
 						{keys.map((k) => {
-							console.log(data[k as keyof typeof data]);
 							return (
 								<td class="border-2">{data[k as keyof typeof data]}</td>
 							);
